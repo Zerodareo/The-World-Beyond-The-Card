e52ef09310adfb3ce457ef7f3187664ef88a4580
Nadhir <95942425+Zerodareo@users.noreply.github.com>
2024-04-12 10:22:32 +0200

    test

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
+const image = {uri: ''};
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      <Text style={styles.text}>Inside</Text>
+    </ImageBackground>
       <StatusBar style="auto" />
+    
     </View>
   );
 }
@@ -13,8 +17,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'cyan',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Text:{
+    fontSize: 30,
+    fontFamily : '',
+    color: 'cyan'
+  }
 });
